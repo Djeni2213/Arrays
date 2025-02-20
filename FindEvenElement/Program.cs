a278f08cfb7af2b98b551f486be917a6ea4f7bf1
K0k0451 <nikola.luchev543@edu1.pgzaimov.com>
2025-02-20 09:41:32 +0200

    20.02 CHANGES

---
+++ FindEvenElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindEvenElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how many numbers you will enter: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n]; 
+
+            //input
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"number[{i}] = ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            //zadacha za otpechatvane na indeksite na chetnite chisla
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0 && i % 2 == 0)
+                {
+                    Console.WriteLine($"{numbers[i]} e na {i} poziciq");
+                }
+            }
+        }
+    }
+}
---
+++ KolkoPutiSeSreshtaChislo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KolkoPutiSeSreshtaChislo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter how many numbers you will enter: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            //input
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"Enter number[{i}]: ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            //Zadacha
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentElement = numbers[i];
+                byte counter = 0;
+                for (int k = 0; k < numbers.Length; k++)
+                {
+                    if (currentElement == numbers[k])
+                    {
+                        counter++;
+                    }
+                }
+                Console.WriteLine($"{currentElement} se sresta {counter} puti");
+            }
+
+
+            //output
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]}, ");
+            }
+
+
+        }
+    }
+}
